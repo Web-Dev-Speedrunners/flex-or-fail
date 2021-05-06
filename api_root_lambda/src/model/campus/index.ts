02893e70570323ea2ac7c208e1f4ad883cf40024
Xuejin Gao <xuejingao@gmail.com>
2021-05-06 13:29:14 -0400

    Added Create and Get All functions to Campus model

---
+++ api_root_lambda/src/model/campus/index.ts
@@ -0,0 +1,55 @@
+import { CampusSequelizeModel } from '../../service/database';
+import GetRandomImageUrl from '../../service/placeholder/get_random_image_url';
+import { CreateCampusValidator } from '../../util/validator';
+
+export type CampusModelCreateProps = {
+  name: string,
+  address: string,
+  description: string,
+  imageUrl?: string,
+}
+
+export default class CampusModel {
+  id: number;
+  name: string;
+  address: string;
+  description: string;
+  imageUrl: string;
+
+  private constructor(dbCampus: CampusSequelizeModel) {
+    this.id = dbCampus.id;
+    this.name = dbCampus.name;
+    this.address = dbCampus.address;
+    this.description = dbCampus.description;
+    this.imageUrl = dbCampus.imageUrl;
+  }
+
+  static async CreateCampus(props: CampusModelCreateProps): Promise<CampusModel> {
+    // Validate the input
+    await CreateCampusValidator.validateAsync(props);
+    // Destructure
+    const {
+      name,
+      address,
+      description
+    } = props;
+    const imageUrl = props.imageUrl? props.imageUrl : await GetRandomImageUrl({
+      width: 400,
+      height: 400
+    });
+
+    const dbCampus = await CampusSequelizeModel.create({
+      name,
+      address,
+      description,
+      imageUrl
+    });
+
+    return new CampusModel(dbCampus);
+  }
+
+  static async GetAll(): Promise<Array<CampusModel>> {
+    const dbCampuses = await CampusSequelizeModel.findAll({ where: {} });
+    return dbCampuses.map((dbCampus) => new CampusModel(dbCampus));
+  }
+}
