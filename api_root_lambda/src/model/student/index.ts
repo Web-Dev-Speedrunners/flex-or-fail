c7db0893175dfebcd6d9efae86851c3adf5658b9
Asif Shikder <shikdernyc@gmail.com>
2021-05-06 00:47:14 -0400

    Added method to create a student and retrieve all students

--- api_root_lambda/src/model/student/index.ts
+++ api_root_lambda/src/model/student/index.ts
@@ -1,18 +1,61 @@
 import { StudentSequelizeModel } from '../../service/database';
+import GetRandomImageUrl from '../../service/placeholder/get_random_image_url';
+import { CreateStudentValidator } from '../../util/validator';
 
-export interface IStudentModel {
-  id: number,
+export type StudentModelCreateProps = {
   firstName: string
+  lastName: string,
+  gpa: number,
+  email: string,
+  imageUrl?: string
 }
 
 export default class StudentModel {
-  static async GetStudentById(id: string) : Promise<IStudentModel> {
-    const databaseEntry = await StudentSequelizeModel.findByPk(id);
-    if (databaseEntry === undefined || databaseEntry === null) throw new Error("Student doesn't exists");
-    const student: IStudentModel = {
-      id: databaseEntry.id,
-      firstName: databaseEntry.firstName,
-    };
-    return student;
+  id: number;
+
+  firstName: string;
+
+  lastName: string;
+
+  gpa: number;
+
+  email: string;
+
+  imageUrl: string;
+
+  private constructor(dbStudent: StudentSequelizeModel) {
+    this.id = dbStudent.id;
+    this.firstName = dbStudent.firstName;
+    this.lastName = dbStudent.lastName;
+    this.email = dbStudent.email;
+    this.gpa = dbStudent.gpa;
+    this.imageUrl = dbStudent.imageUrl;
+  }
+
+  static async CreateStudent(props: StudentModelCreateProps) : Promise<StudentModel> {
+    await CreateStudentValidator.validateAsync(props);
+    const {
+      firstName,
+      lastName,
+      email,
+      gpa,
+    } = props;
+    const imageUrl = props.imageUrl ? props.imageUrl : await GetRandomImageUrl({
+      width: 400,
+      height: 400,
+    });
+    const dbStudent = await StudentSequelizeModel.create({
+      firstName,
+      lastName,
+      email,
+      gpa,
+      imageUrl,
+    });
+    return new StudentModel(dbStudent);
+  }
+
+  static async GetAll() : Promise<StudentModel[]> {
+    const students = await StudentSequelizeModel.findAll({ where: {} });
+    return students.map((dbStudent) => new StudentModel(dbStudent));
   }
 }
