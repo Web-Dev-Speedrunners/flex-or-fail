a51e64c54ba77c783eb445941f02cdf21e90bf9c
Xuejin Gao <xuejingao@gmail.com>
2021-05-08 14:39:19 -0400

    Added Enroll and Unenroll

---
+++ api_root_lambda/src/router_handler/student/enroll.ts
@@ -0,0 +1,18 @@
+import { RequestHandler } from "express";
+import StudentModel from "../../model/student";
+import logger from "../../util/logger";
+
+const EnrollStudentRequestHandler: RequestHandler = async (req, res, next) => {
+  const TAG = EnrollStudentRequestHandler.name;
+  try {
+    const studentId = req.params.studentId;
+    const campusId = req.body.id;
+    await StudentModel.EnrollStudent(studentId, campusId);
+    return res.status(200).json();
+  } catch (err) {
+    logger.error(TAG, err);
+    return next(err);
+  }
+}
+
+export default EnrollStudentRequestHandler;
